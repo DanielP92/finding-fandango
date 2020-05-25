f3699a862ac6ac7bbf5d95a1130ac77347e4f693
Daniel Partridge <45455070+DanielP92@users.noreply.github.com>
2020-05-25 22:10:54 +0100

    small change

--- items.py
+++ items.py
@@ -1,10 +1,10 @@
 import pygame as pg
-from base_classes import SpriteWithCoords, BaseHitBox
+from base_classes import SpriteWithCoords, BaseHitbox
 
 
 class Item(SpriteWithCoords):
     def __init__(self, x, y):
         super().__init__(x, y)
-        self.width, self.height = 10, 16
-        self.hitbox = BaseHitbox(self.x, self.y, self.width, self.height)
+        self.width, self.height = 16, 16
+        self.hitbox = BaseHitbox(self.x, self.y, 10, self.height)
         self.image = pg.Surface(self.width, self.height)
